37bb90d74c61643ab699295a04cc48ac40b85757
Liska-creator <liska.lisu@gmail.com>
2026-03-12 21:19:35 +0400

    Add files via upload

---
+++ films.js
@@ -0,0 +1,32 @@
+const films = [
+    {
+        id: "film1",
+        name: "Avatar",
+        img: "https://m.media-amazon.com/images/M/MV5BZmQzMTMwOTItYjUyMS00NDY4LThmOWEtZjE3ODkyMzg3MDdlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        items: [
+            { id: "avatar1", name: "Avatar Server 1", img: "https://picsum.photos/300/200?11" },
+            { id: "avatar2", name: "Avatar Server 2", img: "https://picsum.photos/300/200?12" }
+        ]
+    },
+    {
+        id: "film2",
+        name: "Titanic",
+        img: "https://inreviewonline.com/wp-content/uploads/2026/02/the-bluff.png",
+        items: [
+            { id: "titanic1", name: "Titanic Server 1", img: "https://picsum.photos/300/200?13" }
+        ]
+    },
+    {
+        id: "film6",
+        name: "Interstellar2",
+        img: "https://picsum.photos/400/300?5",
+        items: [
+            {
+                id: "film62", name: "Interstellar Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
+                link1: "https://picsum.photos/300/200?22",
+                link2: "youtube.com",
+                link3: "youtube.com"
+            }
+        ]
+    }
+];
---
+++ live.js
@@ -0,0 +1,19 @@
+const live = [
+  {
+    id: "live1",
+    name: "BBC News",
+    img: "https://picsum.photos/400/300?6",
+    items: [
+      { id: "bbc1", name: "BBC Server 1", img: "https://picsum.photos/300/200?21" },
+      { id: "bbc2", name: "BBC Server 2", img: "https://picsum.photos/300/200?22" }
+    ]
+  },
+  {
+    id: "live2",
+    name: "Discovery",
+    img: "https://picsum.photos/400/300?7",
+    items: [
+      { id: "discovery1", name: "Discovery Server 1", img: "https://picsum.photos/300/200?23" }
+    ]
+  }
+];
