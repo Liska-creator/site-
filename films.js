866875a646290da208acd92abd7537d97cfb5063
Liska-creator <liska.lisu@gmail.com>
2026-03-12 21:42:09 +0400

    Update films.js

--- films.js
+++ films.js
@@ -1,11 +1,11 @@
 const films = [
     {
-         id: "film1",
-        name: "Avatar",
+         id: "film5",
+        name: "Avata5r5555",
         img: "https://m.media-amazon.com/images/M/MV5BZmQzMTMwOTItYjUyMS00NDY4LThmOWEtZjE3ODkyMzg3MDdlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         items: [
-            { id: "avatar1", name: "Avatar Server 1", img: "https://picsum.photos/300/200?11" },
-            { id: "avatar2", name: "Avatar Server 2", img: "https://picsum.photos/300/200?12" }
+            { id: "avatar4", name: "Avatar Server 5", img: "https://picsum.photos/300/200?11" },
+            { id: "avatar5", name: "Avatar Server 7", img: "https://picsum.photos/300/200?12" }
         ]
     },
     {
@@ -40,3 +40,4 @@ const films = [
     }
 
 ];
+
