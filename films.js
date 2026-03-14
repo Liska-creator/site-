37c679b232efe858d1ed1ed4a91095d1f885f6d0
Liska-creator <liska.lisu@gmail.com>
2026-03-15 00:23:26 +0400

    Add files via upload

---
+++ films.js
@@ -0,0 +1,28 @@
+const films = [
+    {
+        id: "film6",
+        name: "Interstellar2",
+        img: "https://picsum.photos/400/300?5",
+        items: [
+            {
+                id: "film62", name: "Interstellar Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
+                link1: "https://picsum.photos/300/200?22",
+                link2: "youtube.com",
+                link3: "youtube.com"
+            }
+        ]
+    },
+    {
+        id: "film65",
+        name: "Interstellar53",
+        img: "https://picsum.photos/400/300?5",
+        items: [
+            {
+                id: "film625", name: "Interstellar Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
+                link1: "https://picsum.photos/300/200?22",
+                link2: "youtube.com",
+                link3: "youtube.com"
+            }
+        ]
+    }
+];
---
+++ live.js
@@ -0,0 +1,17 @@
+const live = [
+
+  {
+    id: "live4",
+    name: "live5",
+    img: "https://picsum.photos/400/300?5",
+    items: [
+      {
+        id: "live5", name: "live Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
+        link1: "https://picsum.photos/300/200?22",
+        link2: "youtube.com",
+        link3: "youtube.com"
+      }
+    ]
+  },
+ 
+];
