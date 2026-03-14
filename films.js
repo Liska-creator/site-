fc19e3f72253bad777ffbf625dfabbd80b84f783
Liska-creator <liska.lisu@gmail.com>
2026-03-15 00:45:55 +0400

    Update films.js

--- films.js
+++ films.js
@@ -1,17 +1,3 @@
-<!-- {
-        id: "film65",
-        name: "Interstellar53",
-        img: "https://picsum.photos/400/300?5",
-        items: [
-            {
-                id: "film625", name: "Interstellar Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
-                link1: "https://picsum.photos/300/200?22",
-                link2: "youtube.com",
-                link3: "youtube.com"
-            }
-        ]
-    }
---!>
 const films = [
     {
         id: "GinaGerson",
@@ -19,24 +5,24 @@ const films = [
         img: "https://cdn.elitebabes.com/content/170241/0265-05.jpg",
         items: [
             {
-                id: "film62", name: "Gina Gerson And Veronica Leal Lesbian Pleasure And Fisting", img: "https://static-ca-cdn.eporner.com/thumbs/static4/1/12/127/12705285/14_360.jpg",
-                link1: "https://picsum.photos/300/200?22",
-                link2: "youtube.com",
-                link3: "youtube.com"
+                id: "GinaGersonAndVeronicaLeal", name: "Gina Gerson And Veronica Leal Lesbian Pleasure And Fisting", img: "https://static-ca-cdn.eporner.com/thumbs/static4/1/12/127/12705285/14_360.jpg",
+                link1: "https://www.eporner.com/video-pt1MGNgMB4n/gina-gerson-veronica-leal/",
+                link2: "https://www.analdin.com/videos/620394/po/",
+                link3: "https://it.eporner.com/video-TN6VvPg6gIR/two-beautiful-women-have-sex/"
             }
         ]
     },
     {
-        id: "film65",
-        name: "Interstellar53",
-        img: "https://picsum.photos/400/300?5",
+        id: "VeronicaLeal",
+        name: "Veronica Leal",
+        img: "https://s1.milffox.com/p/1/19/33175/pic1.jpg",
         items: [
             {
-                id: "film625", name: "Interstellar Server 5", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ11DtS2HHLba5FaL0Pp7FhOrxi2huOesValmjDLYr-HgS_lI7xLNosgToP1n5kTPm1JDQpLe2SxoCIljKcWK2uCrgVDFCNIpl1dHY7W0Q&s=10",
-                link1: "https://picsum.photos/300/200?22",
-                link2: "youtube.com",
-                link3: "youtube.com"
+                id: "GinaGersonAndVeronicaLeal", name: "Gina Gerson And Veronica Leal Lesbian Pleasure And Fisting", img: "https://static-ca-cdn.eporner.com/thumbs/static4/1/12/127/12705285/14_360.jpg",
+                link1: "https://www.eporner.com/video-pt1MGNgMB4n/gina-gerson-veronica-leal/",
+                link2: "https://www.analdin.com/videos/620394/po/",
+                link3: "https://it.eporner.com/video-TN6VvPg6gIR/two-beautiful-women-have-sex/"
             }
         ]
-    }
+    },
 ];
